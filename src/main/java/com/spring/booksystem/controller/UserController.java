6f31dfbd8aace0501541242955cf60ec5b1f11e0
Hansol Cho <zwwz11@naver.com>
2022-07-14 23:11:22 +0900

    유저 등록폼 유효성 에러 수정중...

--- src/main/java/com/spring/booksystem/controller/UserController.java
+++ src/main/java/com/spring/booksystem/controller/UserController.java
@@ -1,15 +1,50 @@
 package com.spring.booksystem.controller;
 
+import com.spring.booksystem.domain.User;
+import com.spring.booksystem.domain.UserSex;
+import com.spring.booksystem.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
+    @ModelAttribute("userSexes")
+    private UserSex[] userSexes(){
+        return UserSex.values();
+    }
+
     @GetMapping("/register")
-    public String addUser() {
+    public String addUserForm(Model model) {
+        log.info("addUserForm 호출");
+        model.addAttribute("user", new User());
         return "user/addUserForm";
+    }
+
+    @PostMapping("/register")
+    public String addUser(@Validated @ModelAttribute User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("에러 있음");
+            return "user/addUserForm";
+        }
+
+        log.info("유저 저장됨");
+        User savedUser = userService.join(user);
+        return "redirect:localhost:8080/index.html";
     }
 }
